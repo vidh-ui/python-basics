2939503ef95685ccc529faa68f480c1a64e6fd9c
vidh-ui <80629528+vidh-ui@users.noreply.github.com>
2021-03-20 19:15:59 +0530

    Add files via upload

---
+++ ifelse.py.py
@@ -0,0 +1,8 @@
+n = int(input("enter n:"))
+  if n>0:
+     print(n,"is a positive number")
+  else:
+      if n==0:
+          print(n,"is neither positive nor negative")
+      else:
+          print(n,"is a negative number")
