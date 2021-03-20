afd45962c6d202df8815677a0df9f97c72773009
vidh-ui <80629528+vidh-ui@users.noreply.github.com>
2021-03-20 19:48:32 +0530

    Delete andor.py

--- andor.py
+++
@@ -1,9 +0,0 @@
-#check whether the given input is divisible by 2 andor 6
-n = int(input("enter n:"))
-if n%2 == 0 and n%6 == 0:
-    print(n, "is divisible by both 2 and 6")
-elif n%2 == 0 or n%6 ==0:
-    print(n, "is either divisible by 2 or 6")
-else:
-    print(n, "is neither divisible by 2 nor 6")
-
