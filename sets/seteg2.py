883e981f6fda88c1aca40a5c5d82bc7a4bd9700b
vidh-ui <80629528+vidh-ui@users.noreply.github.com>
2021-03-22 18:59:12 +0530

    Add files via upload

---
+++ sets/seteg2.py
@@ -0,0 +1,6 @@
+nos={25,50,100}
+n=sum(nos)
+print(n)
+nos.add(40)
+nos.remove(50)
+print(sum(nos))
---
+++ sets/setseg.py
@@ -0,0 +1,10 @@
+set={'a','b','c','d','e'}
+set.add('f')
+set.add(5)
+set.add("apple")
+print(set)
+
+set.add("mango")
+set.remove('a')
+print(set)
+
