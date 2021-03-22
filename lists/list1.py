c8b1f84d54596991442b3bd6d41d18f3235900ee
vidh-ui <80629528+vidh-ui@users.noreply.github.com>
2021-03-22 18:23:41 +0530

    Add files via upload

---
+++ lists/list1.py
@@ -0,0 +1,11 @@
+animals=["bear","tiger","deer","monkey"]
+print(animals)
+animals[1]="giraffe"
+print(animals)
+animals.append("leapord")
+animals.append("zebra")
+print(animals)
+a=len(animals)
+print(a)
+print (animals[0:3])
+print (animals[3:6])
---
+++ lists/listcomprehension.py
@@ -0,0 +1,5 @@
+nos=[1,2,3,4,5,6,7,8]
+nos1=[(n*n) for n in nos if n>3]
+
+print(nos1)
+
